2135c3fac7206dbccd9adadbd44ac3356fa41233
Victoryia Parcheuskaya <spvika2000@gmail.com>
2018-12-11 21:12:25 +0300

    events.js after help R

--- events.js
+++ events.js
@@ -1,19 +1,20 @@
-            // $(document).ready(function(){
-                
+             $(document).ready(function(){
+                console.log("hello");
                 $.ajax({
                     type:"GET",
                     url:"aboutDali2.xml",
                     dataType:"xml",
                     success:function(xml){
                         //.find('events')
-                        $(xml).find('event').each(function(){
-                            $(this).children().each(function(){
-                                var name=$(this).find("name").text();
-                                var date=$(this).find("date").text();
-                                var place=$(this).find("place").text();
+                        $(xml).find('events').each(function(){
+                            //$(this).children().each(function(){
+                                var events = $(this);
+                                var name=event.find("name").text();
+                                var date=event.find("date").text();
+                                var place=event.find("place").text();
                                 $('<tr><td>' + name + '</td><td>' + date + '</td><td>' + place + '</td></tr>').appendTo("table");
-                            })
+                            //})
                         });
                     }
                 })
-            // });
+             });
